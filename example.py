897a2be2d6a0ba57086a5822b2e12b9bd47fc990
Rafael A. <49209603+rafa-antoniassi@users.noreply.github.com>
2025-04-23 16:25:08 -0300

    Create example.py

---
+++ example.py
@@ -0,0 +1,10 @@
+import os, sys  # má ordenação (isort), F401 (sys não usado)
+
+import json  # espaçamento errado (black)
+
+def somar(a,b):   # espaçamento errado (black), vírgula sem espaço (flake8)
+  print(a+b)  # indentação errada (black), uso de print (flake8-print)
+  print('Debug')  # uso de print (flake8-print)  
+    
+
+somar(1,2)  # trailing whitespace  
